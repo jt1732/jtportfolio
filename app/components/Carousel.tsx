c17e1ed6660cb12df66abb33ef9ad82de70e72fe
Jacob Turnbull <jacob.T.TURNBULL@GMAIL.COM>
2025-03-01 18:05:26 +1300

    Responsive Features

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -11,6 +11,11 @@ const Carousel = () => {
             description:"Test",
             title:"Test",
             subTitle:"Test",
+        },
+        {
+            description:"Test",
+            title:"Test",
+            subTitle:"Test",
         }
     ]
     return (
@@ -19,6 +24,23 @@ const Carousel = () => {
             {items.map((item, index) => {
                 return <CarouselItem key={index} item={item} />;
             })}
+            </div>
+            <div className={styles.arrowButtons}>
+                <button className={styles.buttonArrow}>
+                <span className="material-symbols-outlined">arrow_back_ios_new</span>
+                </button>
+                <div className={styles.indicators}>
+                    {items.map((item,index)=>{
+                        return (
+                            <button className={styles.buttonIndicator}>
+                                <span className="material-symbols-outlined">radio_button_unchecked</span>
+                            </button>
+                        )
+                    })}   
+                </div>
+                <button className={styles.buttonArrow}>
+                    <span className="material-symbols-outlined" >arrow_forward_ios</span>
+                </button>
             </div>
         </div>
     )
